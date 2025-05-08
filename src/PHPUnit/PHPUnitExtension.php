a8f63bcbc16540b2afdb6b96c243b266f32b933f
Luca Manca <luca.manca@facile.it>
2025-05-08 15:39:00 +0200

    upgrade to phpunit 10 using new event system

--- src/PHPUnit/PHPUnitExtension.php
+++ src/PHPUnit/PHPUnitExtension.php
@@ -5,30 +5,45 @@ declare(strict_types=1);
 namespace Facile\DoctrineTestModule\PHPUnit;
 
 use Facile\DoctrineTestModule\Doctrine\DBAL\StaticDriver;
-use PHPUnit\Runner\AfterLastTestHook;
-use PHPUnit\Runner\AfterTestHook;
-use PHPUnit\Runner\BeforeFirstTestHook;
-use PHPUnit\Runner\BeforeTestHook;
+use PHPUnit\Event\Test\BeforeFirstTestMethodCalledSubscriber;
+use PHPUnit\Event\Test\BeforeFirstTestMethodCalled;
+use PHPUnit\Event\Test\BeforeTestMethodCalledSubscriber;
+use PHPUnit\Event\Test\BeforeTestMethodCalled;
+use PHPUnit\Event\Test\AfterTestMethodCalledSubscriber;
+use PHPUnit\Event\Test\AfterTestMethodCalled;
+use PHPUnit\Event\Test\AfterLastTestMethodCalledSubscriber;
+use PHPUnit\Event\Test\AfterLastTestMethodCalled;
+use PHPUnit\Runner\Extension\Extension;
+use PHPUnit\Runner\Extension\Facade;
+use PHPUnit\Runner\Extension\ParameterCollection;
+use PHPUnit\TextUI\Configuration\Configuration;
 
-class PHPUnitExtension implements BeforeFirstTestHook, AfterLastTestHook, BeforeTestHook, AfterTestHook
+class PHPUnitExtension implements Extension
 {
-    public function executeBeforeFirstTest(): void
-    {
-        StaticDriver::setKeepStaticConnections(true);
-    }
-
-    public function executeBeforeTest(string $test): void
-    {
-        StaticDriver::beginTransaction();
-    }
-
-    public function executeAfterTest(string $test, float $time): void
-    {
-        StaticDriver::rollBack();
-    }
-
-    public function executeAfterLastTest(): void
-    {
-        StaticDriver::setKeepStaticConnections(false);
+    public function bootstrap( Configuration $configuration, Facade $facade, ParameterCollection $parameters ): void {
+        $facade->registerSubscriber(new class($this) implements BeforeFirstTestMethodCalledSubscriber {
+            public function notify(BeforeFirstTestMethodCalled $event): void
+            {
+                StaticDriver::setKeepStaticConnections(true);
+            }
+        });
+        $facade->registerSubscriber(new class($this) implements BeforeTestMethodCalledSubscriber {
+            public function notify(BeforeTestMethodCalled $event): void
+            {
+                StaticDriver::beginTransaction();
+            }
+        });
+        $facade->registerSubscriber(new class($this) implements AfterTestMethodCalledSubscriber {
+            public function notify(AfterTestMethodCalled $event): void
+            {
+                StaticDriver::rollBack();
+            }
+        });
+        $facade->registerSubscriber(new class($this) implements AfterLastTestMethodCalledSubscriber {
+            public function notify(AfterLastTestMethodCalled $event): void
+            {
+                StaticDriver::setKeepStaticConnections(false);
+            }
+        });
     }
 }
